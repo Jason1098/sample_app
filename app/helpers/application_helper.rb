d91c334dd4542ac23d1c1fde72e515f93bcf6e6f
jason1098 <jasonbushis@gmail.com>
2012-03-05 01:12:52 -0800

    fixed the messed up merge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
-=======
-    # Returns the full title on a per-page basis.
-    def full_title(page_title)
-      base_title = "Ruby on Rails Jason Sample App"
-      if page_title.empty?
-        base_title
-      else
-        "#{base_title} | #{page_title}"
-      end
-    end
-end
->>>>>>> filling-in-layout
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 SampleApp::Application.routes.draw do
   get "users/new"
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  
 
   match '/signup', to: 'users#new'
   match '/help',    to: 'static_pages#help'
