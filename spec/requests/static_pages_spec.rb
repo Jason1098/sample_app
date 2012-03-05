6db047dd6a4ad0a984ac968bde2d5c392e33230a
Jason Bush <jasonbushis@gmail.com>
2012-03-04 20:31:47 -0800

    changed stylesheet to scss and messed around with the format, logo, colors, etc

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -46,15 +46,16 @@ base_title = "Ruby on Rails Tutorial Sample App"
   
   describe "Contact page" do
 
-      it "should have the h1 'Contact'" do
-        visit '/static_pages/contact'
-        page.should have_selector('h1', :text => 'Contact')
-      end
-
-      it "should have the title 'Contact'" do
-        visit '/static_pages/contact'
-        page.should have_selector('title', :text => "#{base_title} | Contact")
-      end
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
     end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
   
 end
