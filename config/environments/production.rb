6b70de2e5a0782eecaecaffcac29aff83bd637ba
jason1098 <jasonbushis@gmail.com>
2012-03-05 15:39:06 -0800

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ SampleApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   # Precompile Blueprint CSS
-    config.assets.precompile += [ "blueprint/*.css" ]
-  end
+  config.assets.precompile += [ "blueprint/*.css" ]
+  
 end
