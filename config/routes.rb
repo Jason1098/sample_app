51f39d4d095a687ad3d616f6f2e6373a6902ac9b
Jason Bush <jasonbushis@gmail.com>
2012-03-04 21:34:19 -0800

    finished up with the layout for the time being.  fixed the problem in the _footer.css

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
 
-  # get "static_pages/home"
+  get "static_pages/home"
 
-  # get "static_pages/help"
+  get "static_pages/help"
   
-  # get "static_pages/about"
+  get "static_pages/about"
   
-  # get "static_pages/contact"
+  get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
